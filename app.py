a165e2a47a0c4f6b4d533d385440ab427ba67d84
GEOJEDONGGANZINAM <csk2324@gmail.com>
2026-07-07 17:18:10 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ USER_DB = {
     "younghyeun": "3873", 
     "dongwoo": "0788", 
     "kanghyun": "4183", 
-    "student6": "1234", 
+    "jinho": "0727", 
     "student5": "1234", 
     "student5": "1234", 
     "student6": "1234", 
